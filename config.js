bd3c3e7669d8bc5d714d680db67233aeec401793
TuyenPT1 <88217344+Hatuyenpt1@users.noreply.github.com>
2021-10-25 13:16:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Who are you",
-    introTitle: 'Xin chào...tui là Hà Minh Tuyền',
+    introTitle: 'Xin chào...Bạn có phải là Hà Minh Tuyền',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh..
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi Click vào đây^^',
     title: 'Ngay từ cái nhìn đầu tiên, tôi đã bắt đầu có cảm giác với cậu 🥰',
-    desc: 'tôi đã chờ đợi ngày chúng ta bên nhau từ ngày ấy...ý cậu là sao?? ',
+    desc: 'tôi đã chờ đợi ngày chúng ta bên nhau từ lâu lắm rồi...ý cậu là sao?? ',
     btnYes: 'đồng ý <33',
     btnNo: 'Không nha :3',
     question: 'đồ đang ghét...cậu có iu tớ không <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi đến Tuyền <3',
     reply: 'iuu nhất trên đời <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
