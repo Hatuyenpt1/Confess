98e204265a49b065f56452ef02d2f3729b3c8415
TuyenPT1 <88217344+Hatuyenpt1@users.noreply.github.com>
2024-12-24 20:01:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     titleWeb: "Chao cauuuu",
-    introTitle: 'Xin chào...bạn có phải là Huyền đáng iu khong',
+    introTitle: 'Bạn có phải là Huyền đáng iu khong'..Bấm vào bên dưới đi mà, có điều bất ngờ nha!!,
     introDesc: `Chúc bạn có một buổi tối zui zẻ..😄
  `,
     btnIntro: '^^HiHi Click vào đây^^',
-    title: 'bên ấy có iuu bên nàyy khongg?? ',
+    title: 'bên ấy có thick bên nàyy khongg?? ',
     btnYes: 'cóoo <33',
     btnNo: 'Không nha :3',
     question: 'đồ đang ghét...tuii biết bạn bấm có mà <3',
     btnReply: 'Gửi đến Tuyền <3',
-    reply: 'iuu nhất trên đời <33333333',
-    mess: 'mình biết mà  😘😘',
+    reply: 'iuu nhất trên đời <3',
+    mess: 'mình biết bạn viết gì mà 😘😘',
     messDesc: 'hahaa.',
     btnAccept: 'Okiiiii lun <33',
     messLink: 'https://www.facebook.com/HatuyenPt.no1'
