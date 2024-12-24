e7c8044e5eafed3aba6a8e29b0669f7a23fb8cb0
TuyenPT1 <88217344+Hatuyenpt1@users.noreply.github.com>
2024-12-24 19:53:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const CONFIG = {
     introDesc: `Chúc bạn có một buổi tối zui zẻ..😄
  `,
     btnIntro: '^^HiHi Click vào đây^^',
-    title: 'bên ấy có iuu mình khôg?? ',
+    title: 'bên ấy có iuu bên nàyy khongg?? ',
     btnYes: 'cóoo <33',
     btnNo: 'Không nha :3',
     question: 'đồ đang ghét...tuii biết bạn bấm có mà <3',
     btnReply: 'Gửi đến Tuyền <3',
     reply: 'iuu nhất trên đời <33333333',
-    mess: 'mình biết mà 🥰 😘😘',
+    mess: 'mình biết mà  😘😘',
     messDesc: 'hahaa.',
     btnAccept: 'Okiiiii lun <33',
     messLink: 'https://www.facebook.com/HatuyenPt.no1'
